fd662b4fd6c0971ebe587bcc3e5d85e381af07aa
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-01-23 15:23:30 +0300

    Update server.ts align with socket.io

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,24 @@
 import * as platform from "@nucleoidai/platform-express";
-import models from "./src/models";
+
+import { Server } from "socket.io";
 import config from "./config";
+import http from "http";
+import models from "./src/models";
+import { setupSocket } from "./socketHandler";
 
 platform.init(config).then(() => {
   const app = require("./src/app").default;
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: process.env["FRONTEND_URL"] || "http://localhost:5173",
+    },
+  });
   models.init();
-  app.listen(process.env.PORT || 3000);
-});
 
+  setupSocket(io);
+
+  server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  });
+});
