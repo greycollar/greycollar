7a441a9eeb36723caab3acb2a87dbd375509aefd
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-01-28 13:54:07 +0300

    Change api port to 3001

--- api/server.ts
+++ api/server.ts
@@ -18,7 +18,7 @@ platform.init(config).then(() => {
   models.init();
   sessions.setup(io);
 
-  server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  server.listen(process.env.PORT || 3001, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3001}`);
   });
 });
