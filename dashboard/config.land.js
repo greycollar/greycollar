f885d3c5e4ad20ec543429ff542fcb489f32ef6a
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-01-30 18:50:25 +0300

    Update base and redirecturi

--- dashboard/config.land.js
+++ dashboard/config.land.js
@@ -1,12 +1,13 @@
 const config = {
   appId: "10b7bc8c-a49c-4002-b0ec-63599e4b5210",
   name: "GreyCollar",
-  base: "/greycollar",
+  base: "/dashboard",
   beta: true,
   api: "localhost:3000/api",
   project: {
     github: {
-      redirectUri: "https://nuc.land/callback/greycollar",
+      redirectUri:
+        "https://greycollar.gentleflower-99ef02e0.eastus.azurecontainerapps.io/dashboard/callback",
       authUrl: "https://github.com/login/oauth/authorize",
       userUrl: "https://api.github.com/user",
       clientId: "Ov23liDUwCkj5NPpApo7",
