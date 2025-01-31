9f46fabd221a1f5b6723fc63791ab6926ee946ac
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-01-31 17:28:08 +0300

    Create tabs for Integration tab in Colleague

---
+++ dashboard/src/components/IntegrationTabs/IntegrationTabs.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Box, Tab, Tabs } from "@mui/material";
+
+const IntegrationTabs = ({ selectedTab, handleTabChange, tabs }) => {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Tabs
+        value={selectedTab}
+        onChange={handleTabChange}
+        centered
+        sx={{
+          "& .MuiTabs-indicator": {
+            backgroundColor: "primary.main",
+          },
+        }}
+      >
+        {tabs.map((tab, index) => (
+          <Tab
+            key={index}
+            label={tab.title}
+            sx={{
+              "&.Mui-selected": {
+                color: "primary.main",
+              },
+              "&:hover": {
+                color: "primary.main",
+                opacity: 0.7,
+              },
+            }}
+          />
+        ))}
+      </Tabs>
+    </Box>
+  );
+};
+
+export default IntegrationTabs;
