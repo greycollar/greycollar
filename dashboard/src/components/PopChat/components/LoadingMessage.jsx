f235677e2edb4f1b2c39754a5d8461112f832bdc
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-02-08 10:45:57 +0300

    Add margin-top to loading

--- dashboard/src/components/PopChat/components/LoadingMessage.jsx
+++ dashboard/src/components/PopChat/components/LoadingMessage.jsx
@@ -10,6 +10,7 @@ const LoadingMessage = memo(() => (
       height: 50,
       backgroundColor: (theme) => alpha(theme.palette.primary.dark, 0.5),
       borderRadius: 1,
+      mt: 1,
     }}
   >
     <Iconify icon="svg-spinners:tadpole" sx={{ width: 25, height: 25 }} />
