b59a9700dcc72f80663061d8100fb6ab70974e35
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-02-20 19:45:25 +0300

    Update onboard logic for first time user

--- dashboard/src/components/Onboarding/Onboarding.jsx
+++ dashboard/src/components/Onboarding/Onboarding.jsx
@@ -1,27 +1,26 @@
 import LandingDialog from "./LandingDialog";
 import { storage } from "@nucleoidjs/webstorage";
+import { useEvent } from "@nucleoidai/react-event";
 
 import React, { useEffect, useState } from "react";
 
 const Onboarding = () => {
   const landingLevel = storage.get("landingLevel");
+  const [teamSelected] = useEvent("PROJECT_SELECTED", { projectId: null });
 
   const [landing, setLanding] = useState(landingLevel);
 
   useEffect(() => {
-    if (landingLevel !== null) {
-      storage.get("landingLevel");
-    } else if (landingLevel === null) {
+    if (landingLevel === null && teamSelected.projectId !== null) {
       storage.set("landingLevel", 0);
+      setLanding(0);
     }
-  }, []);
+  }, [teamSelected]);
 
   const handleClose = () => {
     storage.set("landingLevel", 1);
     setLanding(1);
   };
-
-  console.log("Landing", landing);
 
   switch (landing) {
     case 0:
