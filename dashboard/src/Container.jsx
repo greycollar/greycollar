fcab56e6b2bba235ea14f11fe17b6cd42b183419
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-02-25 23:00:57 +0300

    Block dialog close if organization not exist

--- dashboard/src/Container.jsx
+++ dashboard/src/Container.jsx
@@ -1,10 +1,12 @@
 import Onboarding from "./components/Onboarding/Onboarding";
 import { Outlet } from "react-router-dom";
 import TeamWizard from "./widgets/TeamWizard/TeamWizard";
+import { useOrganizations } from "./hooks/useOrganizations";
 
 import { publish, useEvent } from "@nucleoidai/react-event";
 
 function Container() {
+  const { organizations } = useOrganizations();
   const [platformDialog] = useEvent("PLATFORM", "PROJECT_BAR_DIALOG", {
     open: false,
   });
@@ -14,10 +16,11 @@ function Container() {
       <TeamWizard
         open={platformDialog.open}
         onClose={() => {
-          publish("PLATFORM", "PROJECT_BAR_DIALOG", { open: false });
+          if (organizations.length > 0) {
+            publish("PLATFORM", "PROJECT_BAR_DIALOG", { open: false });
+          }
         }}
       />
-
       <Onboarding />
       <Outlet />
     </>
