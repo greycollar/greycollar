33392b7c9675aca4ee2575dd7b52961b44774195
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-03-07 09:30:37 +0300

    Create default statistics if team does not have

--- api/src/routes/statistics.ts
+++ api/src/routes/statistics.ts
@@ -1,13 +1,33 @@
-import Dashboard from "../models/Statistics";
+import Statistics from "../models/Statistics";
 import express from "express";
 import platform from "@nucleoidai/platform-express";
-
+import { v4 as uuid } from "uuid";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const { projectId: teamId } = req.session;
 
-  const statistics = await Dashboard.findAll({ where: { teamId } });
+  if (!teamId) {
+    return res.status(400);
+  }
+
+  let statistics = await Statistics.findOne({ where: { teamId } });
+
+  if (!statistics) {
+    const defaultStatistics = {
+      id: uuid(),
+      responseRate: 0,
+      supervisingRate: 0,
+      knowledgeSize: 0,
+      taskCount: 0,
+      totalMessages: 0,
+      teamId: teamId,
+    };
+
+    await Statistics.create(defaultStatistics);
+
+    statistics = await Statistics.findOne({ where: { teamId } });
+  }
 
   res.status(200).json(statistics);
 });
